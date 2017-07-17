8c172bf465316007b2e0b05f85ee3f036a6fa5d6
Cesar Morales Garduño <shell.one.core@gmail.com>
2017-07-17 10:30:47 -0500

    Se implementó el login con los servicios de Twitter. Corrección en la biblioteca de Butterknife

---
+++ app/src/main/java/com/shellcore/android/shelltwitter/main/MainActivity.java
@@ -0,0 +1,15 @@
+package com.shellcore.android.shelltwitter.main;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.shellcore.android.shelltwitter.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
